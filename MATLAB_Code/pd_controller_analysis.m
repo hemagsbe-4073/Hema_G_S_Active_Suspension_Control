5f994c54419c9ed1d76fb224c3a52f573a9c6f87
HEMA G S <hemagsbe@gmail.com>
2026-05-13 21:22:45 +0530

    uploaded matlab code files

---
+++ MATLAB_Code/pd_controller_analysis.m
@@ -0,0 +1,38 @@
+clc;
+clear;
+close all;
+
+% Original suspension system
+num = [1];
+den = [1 3 2];
+
+G = tf(num, den);
+
+% PD Controller parameters
+Kp = 1;
+Kd = 0.5;
+
+% PD Controller
+C = pid(Kp, 0, Kd);
+
+% Closed-loop system
+T = feedback(C*G, 1);
+
+% Plot comparison
+figure;
+
+step(G, 'b', T, 'r', 10);
+grid on;
+
+title('Open Loop vs PD Controlled Suspension System');
+xlabel('Time (seconds)');
+ylabel('Body Displacement');
+
+legend('Open Loop System', 'PD Controlled System');
+title('PD Response | Kp = 1, Kd = 0.5')
+% Performance comparison
+disp('Open Loop Performance:');
+stepinfo(G)
+
+disp('PD Controlled System Performance:');
+stepinfo(T)
